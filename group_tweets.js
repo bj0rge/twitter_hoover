0e23ef72a05f5fabe615899465da98c0b1fdf459
bj0rge <bastien.jorge@gmail.com>
2015-11-29 15:03:49 +0100

    Add: concatenate all the tweets in one file

--- group_tweets.js
+++ group_tweets.js
@@ -15,8 +15,10 @@ function stringStartsWith (str, prefix) {
 
 // All the files are in this directory
 var dirName = './tweets';
+// Tweets will be stored in this variable
 var tweets;
 var files = fs.readdirSync(dirName);
+// This variable will only be used to give informations about the number of files remaining
 var i = 0;
 
 // For each filename
@@ -24,11 +26,22 @@ files.forEach(function(fileName){
     i++;
     // Only on certain files
     if (stringStartsWith(fileName, 'tweets_')){
+        // Reading the file
         var text = fs.readFileSync(dirName + '/' + fileName);
+        // Parsing the text in a JSON
         var tmpTweets = JSON.parse(text);
+        // Concatenating the tweets
         tweets = (typeof tweets == 'undefined') ? tmpTweets : tweets.concat(tmpTweets);
         console.log(fileName + " is read (" + i + "/" + files.length + ")");
     }
 });
 
 console.log("I got " + tweets.length + " tweets, wow!");
+
+console.log("Writing the file, it may takes a few minutes if there are many tweets.")
+var outputFilename = dirName + '/grouped_tweets.json';
+// Writing formated object in a file
+fs.writeFile(outputFilename, JSON.stringify(tweets, null, 4), function(err) {
+    if(err) console.log(err);
+    else console.log("json saved to " + outputFilename);
+});
